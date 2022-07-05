c85930df189ea03c1dad59da9297e5df113f23a8
khewdev <khewdev@gmail.com>
2022-07-06 03:58:01 +0800

    Update .gitignore and go mod init a mock pacakge to prevent errors due to multiple main func in same dir

--- binary-search-tree.go
+++ binary-search-tree.go
@@ -9,31 +9,31 @@ import "fmt"
 var count int
 
 // Node represents the components of a binary search tree
-type Node struct {	
-	Key int
-	Left *Node
+type Node struct {
+	Key   int
+	Left  *Node
 	Right *Node
 }
 
-func (n *Node) Insert (k int) {
+func (n *Node) Insert(k int) {
 	if n.Key < k {
 		// move right
 		if n.Right == nil {
-			n.Right = &Node{Key:k}
+			n.Right = &Node{Key: k}
 		} else {
 			n.Right.Insert(k)
 		}
 	} else if n.Key > k {
 		// move left
 		if n.Left == nil {
-			n.Left = &Node{Key:k}
+			n.Left = &Node{Key: k}
 		} else {
 			n.Left.Insert(k)
 		}
 	}
 }
 
-func (n *Node) Search (k int) bool {
+func (n *Node) Search(k int) bool {
 	count++
 
 	if n == nil {
@@ -51,9 +51,8 @@ func (n *Node) Search (k int) bool {
 	return true
 }
 
-
 func main() {
-	tree := &Node{Key:100}
+	tree := &Node{Key: 100}
 	tree.Insert(200)
 	tree.Insert(300)
 	fmt.Println((tree))
